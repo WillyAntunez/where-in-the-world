7d8553f6cccf414b4b6b04e579010d9abf0f9508
Willy Antunez <willy.e.antunez@gmail.com>
2021-06-11 16:39:17 -0600

    Terminada la llamada a la api para obtener datos de todos los paises al inciar la aplicacion

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,13 @@
 	</div>
 </template>
 
+<script>
+export default {
+	beforeCreate() {
+		this.$store.dispatch('getAllCountries');
+	},
+	methods: {},
+};
+</script>
+
 <style lang="scss"></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		AllCountries: [],
+	},
+	mutations: {
+		setAllCountries(state, payload) {
+			state.AllCountries = payload;
+		},
+	},
+	actions: {
+		async getAllCountries(context) {
+			try {
+				const res = await axios.get(`https://restcountries.eu/rest/v2/all`);
+				const data = await res.data;
+				context.commit('setAllCountries', data);
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	},
+	modules: {},
+});
